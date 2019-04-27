d4fe54fe931ad4643c57c158f07918e171f56c2b
daniel king <danielking@daniels-MacBook-Pro.local>
2019-04-27 13:00:05 -0400

    in progress still

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-blank
+const app = () => {
+  const song = document.querySelector('.song');
+  const play = document.querySelector('.play');
+  const outline = document.querySelector('.moving-outline circle');
+  const video = document.querySelector('.vid-container video');
+
+  //sounds
+  const sounds = document.querySelectorAll('.sound-picker button');
+  //time display
+  const timeDisplay = document.querySelector('.time-display');
+  //get length of svg outline circle
+  const outlineLength = outline.getTotalLength();
+  //duration 
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //play sound
+  play.addEventListener('click', () =>{
+    song.play();
+  });
+};
+
+
+app();
